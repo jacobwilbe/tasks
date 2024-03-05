29473794c03a4e8220bca5abb330d58aace21501
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-03-05 16:08:48 -0500

    coded and tested RevealAnswer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import mark from "./mark.jpg.webp";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
@@ -7,17 +7,21 @@ import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
+import { Button } from "react-bootstrap";
 
 function App(): JSX.Element {
+    const [RevealAnswer, setVisible] = useState<boolean>(false);
+    function flipVisibility(): void {
+        setVisible(!RevealAnswer);
+    }
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Hello World
-            </header>
+            <header className="App-header">UD CISC275 Using State</header>
             <hr></hr>
             <Counter></Counter>
             <hr />
-            <RevealAnswer></RevealAnswer>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {RevealAnswer && <div>42</div>}
             <hr />
             <StartAttempt></StartAttempt>
             <hr />
