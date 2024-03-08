63fba84b6219471985ed34d6c5af740c743cf332
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-03-08 13:35:11 -0300

    coded and tested multpile choice

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selected, setSelected] = useState<string>(options[0]);
+    function updateSelected(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelected(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="Multiple Choice">
+                <Form.Label>Choose your answer</Form.Label>
+                <Form.Select value={selected} onChange={updateSelected}>
+                    {options.map((op: string) => (
+                        <option key={op} value={op}>
+                            {op}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {selected === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
